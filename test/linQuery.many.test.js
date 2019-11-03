17500d5a2766ec638c2e3b3effc7fa7a5938470c
xing.pan <selypan@foxmail.com>
2019-11-04 01:52:35 +0800

    type many

---
+++ test/linQuery.many.test.js
@@ -0,0 +1,45 @@
+const assert = require('assert');
+require('../index');
+const getCollectionByName = require('./db');
+
+
+let PeopleConn;
+let BlogConn;
+before(async function () {
+  PeopleConn = await getCollectionByName('people');
+  BlogConn = await getCollectionByName('blog');
+  const { insertedIds } = await BlogConn.insertMany([
+    { title: `blogTitle ${ new Date() }` },
+    { title: `blogTitle ${ new Date() }` },
+    { title: `blogTitle ${ new Date() }` },
+    { title: `blogTitle ${ new Date() }` },
+  ]);
+  await PeopleConn.insertOne({ name: `name ${ new Date() }`, blogIds: Object.values(insertedIds) });
+});
+
+describe('linkQuery many', function () {
+  it('link many正常', async () => {
+    PeopleConn.removeLinks();
+    BlogConn.removeLinks();
+
+    PeopleConn.addLinks({
+      blog: {
+        collection: BlogConn,
+        field: 'blogIds',
+        type: 'many',
+        index: true,
+      },
+    });
+
+    const res = await PeopleConn.linkQuery({
+      $options: {
+        sort: { _id: -1 }
+      },
+      blog: {
+        title: 1
+      },
+    }).fetchOne();
+
+    assert.ok(res.blogIds.length === res.blog.length)
+  });
+});
