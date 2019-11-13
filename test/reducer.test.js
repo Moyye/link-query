ed30c58244745168503250f4fafd5968a18e82fa
Wang, Tianran <tianran.wang@sap.com>
2019-11-13 13:54:57 +0800

    Add test for reducer

--- test/reducer.test.js
+++ test/reducer.test.js
@@ -1,41 +1,96 @@
 const assert = require('assert');
-require('../index');
 const getCollectionByName = require('./db');
+require('../index');
 
 
 describe('Reducer test', function () {
-    it('test add reducer one', async () => {
-        // 1. init db connector
-        let UserConn = await getCollectionByName('user');
-        // 2. add reducer
-        UserConn.addReducer({
-            fullName: {
-                body: {
-                    firstName: 1,
-                    lastName: 1,
-                },
-                reduceFunction(object) {
-                    let {
-                        firstName,
-                        lastName,
-                    } = object
-                    return `${firstName} ${lastName}`
-                },
-            }
+  it('test add reducer one', async () => {
+    // 1. init db connector
+    let UserConn = await getCollectionByName('user');
+    // 2. add reducer
+    UserConn.addReducer({
+      fullName: {
+        body: {
+          firstName: 1,
+          lastName: 1,
+        },
+        reduceFunction(object) {
+          let {
+            firstName,
+            lastName,
+          } = object
+          return `${firstName} ${lastName}`
+        },
+      }
 
-        })
-        // 3. insert data
-        let userInfo = {
-            firstName: 'Hari',
-            lastName: 'Seldon',
-        }
-        await UserConn.insertOne(userInfo)
-        // 4. query and fetch
-        let user = await UserConn.linkQuery({
-            firstName: 1,
-            lastName: 1,
-        }).fetchOne()
-        // 5. compare result
-        assert.ok(user.fullName == `${userInfo.firstName} ${userInfo.lastName}`)
+    })
+    // 3. insert data
+    let userInfo = {
+      firstName: 'Hari',
+      lastName: 'Seldon',
+    }
+    await UserConn.insertOne(userInfo)
+    // 4. query and fetch
+    let user = await UserConn.linkQuery({
+      firstName: 1,
+      lastName: 1,
+    }).fetchOne()
+    // 5. compare result
+    assert.ok(user.fullName == `${userInfo.firstName} ${userInfo.lastName}`)
+  })
+  it('test add reducer many', async () => {
+    // 1. init db connector
+    let UserConn = await getCollectionByName('user');
+    // 2. add reducer
+    UserConn.addReducer({
+      fullName: {
+        body: {
+          firstName: 1,
+          lastName: 1,
+        },
+        reduceFunction(object) {
+          let {
+            firstName,
+            lastName,
+          } = object
+          return `${firstName} ${lastName}`
+        },
+      },
+      nickname: {
+        body: {
+          firstName: 1,
+          lastName: 1,
+        },
+        reduceFunction(object) {
+          let {
+            firstName,
+            lastName,
+          } = object
+          return `nickname $$${firstName} ${lastName}$$`
+        },
+      }
+    })
+    // 3. insert data
+    // TODO: remove insert data and add texture for test
+    let userInfo = [
+      {
+        firstName: 'Hari',
+        lastName: 'Seldon',
+      }, {
+        firstName: 'Gaal',
+        lastName: 'Dornick',
+      }, 
+    ]
+    await UserConn.insertMany(userInfo)
+    // 4. query and fetch
+    let users = await UserConn.linkQuery({
+      firstName: 1,
+      lastName: 1,
+    }).fetch()
+    // 5. compare result
+    users.forEach(user => {
+      assert.ok(user.fullName == `${user.firstName} ${user.lastName}`)
+      assert.ok(user.nickname == `nickname $$${user.firstName} ${user.lastName}$$`)
     });
+  })
 });
